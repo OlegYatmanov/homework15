168d8376ee2ae39e028be1c0f3a9646620d3eccc
Oleg Yatmanov <o_yatmanov@mail.ru>
2023-07-08 18:37:57 +0500

    Семинар 15 домашнее задание

---
+++ task64/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введи M: ");
+int M = int.Parse(ReadLine());
+Write("Введи N: ");
+int N = int.Parse(ReadLine());
+WriteLine($"N={M}, M={N}");
+if (M>N)
+for (int i = N; i <= M; i++)
+    Write($" {i}");
+else
+    for (int i = M; i <= N; i++)
+        Write($" {i}");
---
+++ task66/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+int m = InputNumber("Введите M:");
+int n = InputNumber("Введите N:");
+WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
+
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalSum(m, n - 1);
+}
+
+int InputNumber(string message)
+{
+    WriteLine(message);
+    return int.Parse(ReadLine());
+}
